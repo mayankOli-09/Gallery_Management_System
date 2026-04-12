148600cbe6461600d84992ae96a66a547bad7f84
mayankOli-09 <240211193@geu.ac.in>
2026-04-12 23:51:15 +0530

    Create gallerydisplay.h

---
+++ gallerydisplay.h
@@ -0,0 +1,13 @@
+#ifndef GALLERYDISPLAY_H
+#define GALLERYDISPLAY_H
+
+#include <string>
+#include "gallery.cpp"
+using namespace std;
+
+class GalleryDisplay : public Gallery {
+public:
+    void displayGallery();
+};
+
+#endif
